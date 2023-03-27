46698e87b747364909311c9eb8a37af8daf80a65
Andrei <106434349+AndreyLebedev99@users.noreply.github.com>
2023-03-27 17:51:00 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,18 +8,5 @@ $(function() {
 		byRow: false,
 	});
 
-	// new WOW().init();
-
-	$('.scroll-link').click(function (event) {
-		event.preventDefault();
-		var id = $(this).attr('href'),
-			top = $(id).offset().top;
-		$('body,html').animate({ scrollTop: top }, 500);
-		$('.header__block').removeClass('active');
-		$('html, body').removeClass('not-scroll');
-	});
-
-
-
-   
+	// new WOW().init();   
 });
