8411ed89ae04c80e7d187c70d4283f0c256549d5
Andrei <106434349+AndreyLebedev99@users.noreply.github.com>
2023-03-29 12:02:31 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(function() {
-
 	$('.possibility__title').matchHeight({
 		byRow: false,
 	});
@@ -7,102 +6,5 @@ $(function() {
 	$('.possibility__description').matchHeight({
 		byRow: false,
 	});
-
-	// new WOW().init();
-
-	$('.btn_hover')
-    .on('mouseenter', function(e) {
-		var parentOffset = $(this).offset(),
-  		relX = e.pageX - parentOffset.left,
-  		relY = e.pageY - parentOffset.top;
-		$(this).find('span').css({top:relY, left:relX})
-    })
-    .on('mouseout', function(e) {
-		var parentOffset = $(this).offset(),
-  		relX = e.pageX - parentOffset.left,
-  		relY = e.pageY - parentOffset.top;
-		$(this).find('span').css({top:relY, left:relX})
-    });
-
-	$('.scroll-link').click(function(event) {
-		event.preventDefault();
-		var id  = $(this).attr('href'),
-		top = $(id).offset().top;
-		$('body,html').animate({scrollTop: top}, 500);
-		$('.header__block').removeClass('active');
-		$('html, body').removeClass('not-scroll');
-	});
-
-	$('.for__step_1 .for__items a').click(function() {
-		if ($(this).hasClass('active')) {
-			$(this).siblings().removeClass('transparent');
-			$(this).removeClass('active');
-			$('.direction').fadeOut(300);
-			$('.for__step_2, .for__step_2 .for__items a').removeClass('active').removeClass('transparent');
-		} else {
-			$(this).siblings().removeClass('active').addClass('transparent');
-			$(this).removeClass('transparent').addClass('active');
-			$('.for__step_2 .for__items a').removeClass('active').removeClass('transparent');
-			let direction = $(this).attr('href');
-			$('.direction').hide();
-			$(direction).fadeIn(300);
-			$('.for__step_2').addClass('active');
-		}
-		$('.for__step_3').fadeOut(300);
-		return false;
-	});
-
-	$('.for__step_2 .for__items a').click(function() {
-		if ($(this).hasClass('active')) {
-			$(this).siblings().removeClass('transparent');
-			$(this).removeClass('active');
-			$('.for__step_3').fadeOut(300);
-		} else {
-			$(this).siblings().removeClass('active').addClass('transparent');
-			$(this).removeClass('transparent').addClass('active');
-			$('.for__step_3').fadeIn(300);
-		}
-		return false;
-	});
-
-	$('.modal-link').click(function() {
-		let modal = $(this).attr('href');
-		$(modal).fadeIn(300);
-		return false;
-	});
-
-	$('.modal__close, .modal__blackout').click(function() {
-		$(this).parents('.modal-wrapper').fadeOut(300);
-		return false;
-	});
-
-	$('.header__navbar').click(function() {
-		$('.header__block').addClass('active');
-		$('html, body').addClass('not-scroll');
-		return false;
-	});
-
-	$('.header__block-close').click(function() {
-		$('.header__block').removeClass('active');
-		$('html, body').removeClass('not-scroll');
-		return false;
-	});
-
-
-
-	$(window).scroll(function() {
-		if ($(window).scrollTop() > ($('.step_1').offset().top - $(window).height())) {
-			let x = $(window).scrollTop() - $('.step_1').offset().top;
-			$('.step_1 .step__signature').css('margin-right', x+'px');
-		}
-		if ($(window).scrollTop() > ($('.step_2').offset().top - $(window).height())) {
-			let x2 = $(window).scrollTop() - $('.step_2').offset().top;
-			$('.step_2 .step__signature').css('margin-left', x2+'px');
-		}
-		if ($(window).scrollTop() > ($('.step_3').offset().top - $(window).height())) {
-			let x3 = $(window).scrollTop() - $('.step_3').offset().top;
-			$('.step_3 .step__signature').css('margin-right', x3+'px');
-		}
-	})
-    
+	// new WOW().init();   
 });
--- js/utils/scrollWindow.js
+++ js/utils/scrollWindow.js
@@ -7,24 +7,24 @@ $(function () {
 		$(`${className} .step__signature`).css(`${side}`, x + 'px');
 	}
 
-	// $(window).scroll(function () {
-	// 	let steps = [
-	// 		$('.step_1').offset().top,
-	// 		$('.step_2').offset().top,
-	// 		$('.step_3').offset().top
-	// 	]
-	// 	currentPos = $(window).scrollTop();
-	// 	if (currentPos > (steps[0] - windowHeight) && currentPos < (steps[0] + windowHeight)) {
-	// 		changeMargin('.step_1', steps[0])
-	// 	}
-	// 	if (currentPos > (steps[1] - windowHeight) && currentPos < (steps[1] + windowHeight)) {
+	$(window).scroll(function () {
+		let steps = [
+			$('.step_1').offset().top,
+			$('.step_2').offset().top,
+			$('.step_3').offset().top
+		]
+		currentPos = $(window).scrollTop();
+		if (currentPos > (steps[0] - windowHeight) && currentPos < (steps[0] + windowHeight)) {
+			changeMargin('.step_1', steps[0])
+		}
+		if (currentPos > (steps[1] - windowHeight) && currentPos < (steps[1] + windowHeight)) {
 
-	// 		changeMargin('.step_2', steps[1], 'left')
-	// 	}
-	// 	if (currentPos > (steps[2] - windowHeight) && currentPos < (steps[2] + windowHeight)) {
+			changeMargin('.step_2', steps[1], 'left')
+		}
+		if (currentPos > (steps[2] - windowHeight) && currentPos < (steps[2] + windowHeight)) {
 
-	// 		changeMargin('.step_3', steps[2])
+			changeMargin('.step_3', steps[2])
 			
-	// 	}
-	// })
+		}
+	})
 })
