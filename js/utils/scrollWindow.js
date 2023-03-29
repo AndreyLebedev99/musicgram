dca960fbc002873258494382c849c22bd8519335
Andrei <106434349+AndreyLebedev99@users.noreply.github.com>
2023-03-29 12:16:58 +0300

    Add files via upload

--- js/utils/scrollWindow.js
+++ js/utils/scrollWindow.js
@@ -17,13 +17,13 @@ $(function () {
 		if (currentPos > (steps[0] - windowHeight) && currentPos < (steps[0] + windowHeight)) {
 			changeMargin('.step_1', steps[0])
 		}
-		if (currentPos > (steps[1] - windowHeight) && currentPos < (steps[1] + windowHeight)) {
+		// if (currentPos > (steps[1] - windowHeight) && currentPos < (steps[1] + windowHeight)) {
 
-			changeMargin('.step_2', steps[1], 'left')
-		}
+		// 	changeMargin('.step_2', steps[1], 'left')
+		// }
 		if (currentPos > (steps[2] - windowHeight) && currentPos < (steps[2] + windowHeight)) {
 
-			changeMargin('.step_3', steps[2])
+			changeMargin('.step_3', steps[2], 'left')
 			
 		}
 	})
